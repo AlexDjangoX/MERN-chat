37297d9c9678fa3d15522b8658bcc5e3370747d2
AlexDjangoX <alexmonk17@gmail.com>
2022-09-26 09:22:09 +0100

    Created schema and models

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Container, Box, Text } from '@chakra-ui/react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW='xl' centerContent>
+      <Box
+        d='flex'
+        justifyContent='center'
+        p={3}
+        bg={'white'}
+        w='100%'
+        m='40px 0 15px 0'
+        borderRadius='1g'
+        borderWidth='1px'
+      >
+        <Text>Talk-Polish</Text>
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
