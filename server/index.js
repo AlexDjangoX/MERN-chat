6dd8abaa2f0190f1c9e8a4e0dbe02ddaa0c7a067
AlexDjangoX <alexmonk17@gmail.com>
2022-09-28 14:41:47 +0100

    Added error handling middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import colors from 'colors';
 import userRoutes from './routes/userRoutes.js';
+import { notFound } from './middleware/errorMiddleware.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
 dotenv.config();
@@ -16,5 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server hot on PORT: ${PORT}`.yellow.italic));
