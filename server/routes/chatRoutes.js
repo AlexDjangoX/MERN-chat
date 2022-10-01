5e055acfb11e81b4741353db27d95b1a59aff714
AlexDjangoX <alexmonk17@gmail.com>
2022-10-01 09:01:08 +0100

    Create new group chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { accessChat, fetchChats } from '../controllers/chatControllers.js';
+import {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+} from '../controllers/chatControllers.js';
 const router = express.Router();
 
 router.post('/', protect, accessChat);
 router.get('/', protect, fetchChats);
-// router.post('/group', protect, createGroupChat);
+router.post('/group', protect, createGroupChat);
 // router.put('/group-rename', protect, renameGroup);
 // router.put('/group-remove', protect, removeFromGroup);
 // router.put('/group-add', protect, addToGroup);
